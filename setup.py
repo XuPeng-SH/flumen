9a5084906dd8fc79acd366a1ef74c71b58083fbe
Xu Peng <xupeng@tictalk.com>
2018-12-08 00:54:36 +0800

    feat(pack): add some pack info

--- setup.py
+++ setup.py
@@ -3,6 +3,13 @@ from setuptools import setup
 setup(
     name='flumen',
     version='0.1',
+    description=(
+        'Event stream framework'
+    ),
+    long_description=open('README.md').read(),
+    author='Xu Peng',
+    author_email='xupeng3112@163.com',
+    url='https://github.com/pangyun/flumen',
     packages=['flumen', 'demo'],
     include_package_data=True
 )
